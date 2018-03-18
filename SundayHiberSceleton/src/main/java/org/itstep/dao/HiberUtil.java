902486ae047db57ecb725573ed3918ec2a3a4274
Alex Ignatenko <alex.ignatenko@scalors.com>
2018-03-18 13:45:25 +0200

    Made properties config

--- SundayHiberSceleton/src/main/java/org/itstep/dao/HiberUtil.java
+++ SundayHiberSceleton/src/main/java/org/itstep/dao/HiberUtil.java
@@ -2,13 +2,21 @@ package org.itstep.dao;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.itstep.model.Account;
+import org.itstep.model.Good;
+import org.itstep.model.GoodAction;
 
 class HiberUtil {
 
 	private static SessionFactory sessionFactory = createSessionFactory();
 
 	private static SessionFactory createSessionFactory() {
-		Configuration configuration = new Configuration().configure();
+		Configuration configuration = new Configuration();
+		
+		configuration.addAnnotatedClass(Account.class);
+		configuration.addAnnotatedClass(Good.class);
+		configuration.addAnnotatedClass(GoodAction.class);
+		
 		return configuration.buildSessionFactory();
 	}
 	
